e6f3162e44b9a6a733b1c83985e822716da9bb07
Jorge Netto <jorgenetto96@gmail.com>
2022-12-22 22:14:56 -0300

    Linktree finalizado

--- src/components/RedesSociais/RedesSociais.js
+++ src/components/RedesSociais/RedesSociais.js
@@ -2,18 +2,25 @@ import './RedesSociais.css';
 
 const RedesSociais = () => {
 
-    const ListaRedesSociais = [ 
+    const ListaRedes = [ 
         { id: 1, urlNome: 'Instagram' , url: 'https://www.instagram.com/jorgenetto.dev/'},
-        { id: 2, urlNome: 'Linkedin' , url: 'www.linkedin.com/in/jorgenettodev'},
+        { id: 2, urlNome: 'Linkedin' , url: 'https://www.linkedin.com/in/jorgenettodev'},
         { id: 3, urlNome: 'Github' , url: 'https://github.com/jorgenettodev'},
         { id: 4, urlNome: 'Whatsapp' , url: 'https://api.whatsapp.com/send?phone=5582999167727'},
     ]
 
-
+    const LinksRedes = ListaRedes.map( (props) => 
+        <div key={props.id} className='conteudo_links'>
+            <a href={props.url}>{props.urlNome}</a>
+        </div>
+    
+    )
 
 
     return (
-        <></>
+        <>
+            {LinksRedes}
+        </>
     )
 }
 
