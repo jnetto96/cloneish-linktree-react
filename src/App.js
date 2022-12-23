df3d5f20944ab8fd8062b23fa79e74ea9c621c15
Jorge Netto <jorgenetto96@gmail.com>
2022-12-22 21:25:37 -0300

    primeiras mudancas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header';
+import RedesSociais from './components/RedesSociais/RedesSociais';
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <RedesSociais />
+
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="perfil_conteudo">
+            <div className="perfil_imagem"></div>
+            <h3>Jorge Netto</h3>
+            <p>Desenvolvedor Front End</p>
+        </div>
+    )
+}
+
+export default Header
